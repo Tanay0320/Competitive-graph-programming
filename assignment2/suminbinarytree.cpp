683b6930331b2ecf8ffa4d4e99de8329403fcc05
Tanay0320 <tanaypathode49@gmail.com>
2024-05-29 17:49:59 +0530

    Add files via upload

---
+++ assignment2/suminbinarytree.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void printsumoftree(long long int i,long long int &sum ){
+    sum=sum+(i+1);    
+    if(i==0){
+        return;
+    }                                           
+     printsumoftree((i-1)/2,sum);
+
+}
+
+int main(){
+    int N;
+    long long int x;
+    cin>>N;
+    vector<long long int>arr;
+    for(int i=0;i<N;i++){
+        cin>>x;
+        arr.push_back(x);
+    }
+    for(int i=0;i<N;i++){
+    long long int sum=0;
+    printsumoftree(arr[i]-1,sum);
+    cout<<sum<<endl;
+    }
+    return 0;
+}
